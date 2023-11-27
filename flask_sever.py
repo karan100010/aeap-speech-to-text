f7ac226e092901167f0e807831a8977c0c9ec619
karan100010 <karanjoshi1993@gmail.com>
2023-11-27 16:41:21 +0530

    x

--- flask_sever.py
+++ flask_sever.py
@@ -3,7 +3,22 @@ import wave
 import audioop
 import json
 app = Flask(__name__)
+def convert_file(file):
+    # Decode and combine u-law fragments into a single bytearray
+    combined_pcm_data = bytearray()
+    ulaw_data = bytes(file['data']['data'])
 
+
+    # Decode the u-law data to 16-bit linear PCM
+    pcm_data = audioop.ulaw2lin(ulaw_data, 2)
+   
+
+    # Save the combined PCM data to a WAV file
+    with wave.open('output.wav', 'wb') as wf:
+        wf.setnchannels(1)  # Adjust based on the number of channels in your audio
+        wf.setsampwidth(2)  # 2 bytes for 16-bit audio
+        wf.setframerate(8000)  # Adjust based on the sample rate of your u-law audio
+        wf.writeframes(pcm_data)
 @app.route('/convert', methods=['POST'])
 def convert_ulaw_to_wave():
 
@@ -47,19 +62,3 @@ if __name__ == '__main__':
     #run on port 5005
     app.run(host='localhost', port=5005)
 
-def convert_file(file):
-    # Decode and combine u-law fragments into a single bytearray
-    combined_pcm_data = bytearray()
-    ulaw_data = bytes(file['data']['data'])
-
-
-    # Decode the u-law data to 16-bit linear PCM
-    pcm_data = audioop.ulaw2lin(ulaw_data, 2)
-   
-
-    # Save the combined PCM data to a WAV file
-    with wave.open('output.wav', 'wb') as wf:
-        wf.setnchannels(1)  # Adjust based on the number of channels in your audio
-        wf.setsampwidth(2)  # 2 bytes for 16-bit audio
-        wf.setframerate(8000)  # Adjust based on the sample rate of your u-law audio
-        wf.writeframes(pcm_data)
